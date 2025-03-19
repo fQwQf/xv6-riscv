d16924b02acd5d5861317e8cb54b984289dae610
fQwQf <supertjz123@foxmail.com>
2025-03-19 14:33:11 +0800

    添加 RISC-V QEMU 启动脚本

---
+++ run.sh
@@ -0,0 +1,2 @@
+qemu-system-riscv64 -d guest_errors -D qemu.log -machine virt,accel=tcg -bios none -kernel kernel/kernel -m 128M -smp 3 -nographic -global virtio-mmio.force-legacy=false -drive file=fs.img,if=none,format=raw,id=x0 -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0
+
